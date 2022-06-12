4beac5c32e658ed4a2c029b4299bec6beefbc483
Vane <cwq.banhai@outlook.com>
2022-06-12 18:17:37 +0800

    Add files via upload

---
+++ module5/sliding_window_counters.go
@@ -0,0 +1,79 @@
+package limiter
+
+import (
+	"sync"
+	"time"
+)
+
+type slot struct {
+	timestamp time.Time
+	count     int
+}
+
+type SlidingWindow struct {
+	mu              sync.Mutex // 互斥锁保护其他字段
+	SlotDuration    time.Duration
+	WindowsDuration time.Duration
+	slotsCount      int
+	slots           []*slot
+	maxReqNum       int
+}
+
+//清理过期小窗口
+func (l *SlidingWindow) cleanExpiredSlot(now time.Time) {
+	offset := -1
+	needClean := false
+	for i, v := range l.slots {
+		if v.timestamp.Add(l.WindowsDuration).After(now) {
+			needClean = true
+			break
+		}
+		offset = i
+	}
+	if needClean {
+		l.slots = l.slots[offset+1:]
+	}
+}
+
+func (l *SlidingWindow) countReq() (count int) {
+	for _, v := range l.slots {
+		count += v.count
+	}
+	return
+}
+
+func (l *SlidingWindow) AllowRequest() bool {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	now := time.Now()
+	// 清理时间窗中过期slot
+	l.cleanExpiredSlot(now)
+	// 判断请求是否超限
+	if l.countReq() >= l.maxReqNum {
+		return false
+	}
+	var last *slot
+	if len(l.slots) > 0 {
+		last = l.slots[len(l.slots)-1]
+		// 判断当前时间是否超出槽位范围 如超出 则新建
+		if last.timestamp.Add(l.SlotDuration).Before(now) {
+			last = &slot{timestamp: now, count: 1}
+			l.slots = append(l.slots, last)
+		} else {
+			last.count++
+		}
+	} else {
+		last = &slot{timestamp: now, count: 1}
+		l.slots = append(l.slots, last)
+	}
+	return true
+}
+
+func NewSlidingWindow(slot time.Duration, windows time.Duration, maxReqNum int) *SlidingWindow {
+	return &SlidingWindow{
+		SlotDuration:    slot,
+		WindowsDuration: windows,
+		slotsCount:      int(windows / slot),
+		maxReqNum:       maxReqNum,
+	}
+}
